23bddb7edf583999091158396285888d7b44478b
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-05-29 18:55:53 +0300

    hw-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,15 @@
-function checkForSpam(message) {
-  const blacklistedWord1 = "spam";
-  const blacklistedWord2 = "sale";
-  const messageLowerCase = message.toLowerCase();
-  const result =
-    messageLowerCase.includes(blacklistedWord1) ||
-    messageLowerCase.includes(blacklistedWord2);
-  return result;
+function filterArray(numbers, value) {
+  const maxArray = [];
+  for (const number of numbers) {
+    if (number > value) {
+      maxArray.push(number);
+    }
+  }
+  return maxArray;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
